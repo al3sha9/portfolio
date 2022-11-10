1e8c8b7840cbd3994cb7d2802c3a6bd69f1d820e
al3sha9 <optimist.alishan@gmail.com>
2022-11-10 12:16:37 +0500

    toggle image

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -20,6 +20,20 @@ function NAVBAR({ name, ...props }) {
   new Darkmode().showWidget();
 
 
+  const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '♡', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+
 const darkmode =  new Darkmode();
 darkmode.toggle();
 console.log(darkmode.isActivated()) 
